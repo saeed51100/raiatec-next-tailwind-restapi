1787e0762873b9da3d196d2384cf2733930dbfa5
saeed <saeedblack77@gmail.com>
2023-07-15 19:34:19 +0330

    khooneh

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { getPosts } from "api/getPosts";
+import {useEffect} from "react";
 
 const IndexPage = () => {
     const [posts, setPosts] = useState([]);
