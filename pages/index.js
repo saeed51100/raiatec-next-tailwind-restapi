056c58bb51c682247923bbd5ade75befb6c32085
saeed <saeedblack77@gmail.com>
2023-07-16 09:45:07 +0330

    edare

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,30 @@
 // pages/index.js
+import React, { useState, useEffect } from "react";
+import swr from "swr";
+import { getPosts } from "api/getPosts";
+
+const Posts = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const { data } = await getPosts();
+            setPosts(data);
+        };
+        fetchPosts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            {posts.map((post, index) => (
+                <div key={index}>
+                    <h2>{post.title}</h2>
+                    <p>{post.content}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Posts;
