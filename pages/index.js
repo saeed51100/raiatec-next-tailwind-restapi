0c99c605a71717611b60c075783dd0c493bf4a5b
saeed <saeedblack77@gmail.com>
2023-07-17 09:32:37 +0330

    edare

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
-// pages/index.js
-import React, { useState, useEffect } from "react";
-import { getPosts } from "api/getPosts";
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Posts = () => {
-    const [posts, setPosts] = useState([]);
+class Posts extends Component {
+    state = {
+        posts: [],
+    };
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const { data } = await getPosts();
-            setPosts(data);
-        };
-        fetchPosts();
-    }, []);
+    componentDidMount() {
+        axios.get('http://localhost/test.raiatec.com/wp-json/wp/v2/posts')
+            .then(response => {
+                this.setState({
+                    posts: response.data,
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-    return (
-        <div>
-            <h1>Posts</h1>
-            {posts.map((post, index) => (
-                <div key={index}>
-                    <h2>{post.title}</h2>
-                    <p>{post.content}</p>
-                </div>
-            ))}
-        </div>
-    );
-};
+    render() {
+        return (
+            <div>
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.content}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
 
 export default Posts;
