937e863e734e0a089221538d521435d2e2b7f0b1
saeed <saeedblack77@gmail.com>
2023-07-18 07:58:11 +0330

    edare

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import useSWR from 'swr';
 
 const apiUrl = 'http://localhost/test.raiatec.com/wp-json/wp/v2/posts';
 
@@ -16,16 +17,30 @@ const getPosts = async () => {
     }
 };
 
-const posts = await getPosts();
-
 const Posts = () => {
-  return (
-    <div>
-      {posts.map((post) => (
-        <h2 key={post.id}>{post.title.rendered}</h2>
-      ))}
-    </div>
-  );
+    const {data, error, isLoading} = useSWR(
+        apiUrl,
+        getPosts,
+        {
+            revalidate: 10000,
+            staleWhileRevalidate: true,
+            defer: true,
+        } // This part has been added for optimization. please control it later. saeed.doc
+    );
+
+    if (error) {
+        return <div>Error fetching posts</div>;
+    } else if (isLoading) {
+        return <div>Loading posts...</div>;
+    } else {
+        return (
+            <div>
+                {data.map((post) => (
+                    <h2 key={post.id}>{post.title.rendered}</h2>
+                ))}
+            </div>
+        );
+    }
 };
 
 export default Posts;
