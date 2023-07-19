f617e3b0d9581fe05c6ee46770f8392d5d735cf3
saeed <saeedblack77@gmail.com>
2023-07-19 18:21:13 +0330

    khooneh

---
+++ pages/index1.js
@@ -0,0 +1,14 @@
+
+// import Navbar from 'components/navbar';
+import Grid from 'components/grid';
+// import Footer from 'components/footer';
+
+export default function IndexShow() {
+    return (
+        <div>
+            {/*<Navbar />*/}
+            <Grid />
+            {/*<Footer />*/}
+        </div>
+    );
+}
