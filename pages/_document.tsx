66c6b530f06fbfce4c3f49c9c054b45e818d1be5
saeed <saeedblack77@gmail.com>
2023-07-20 18:39:59 +0330

    khooneh

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html className="h-full bg-white">
+            <Html dir="rtl" className="h-full bg-white">
                 <Head />
                 <body className="h-full">
                 <Main />
