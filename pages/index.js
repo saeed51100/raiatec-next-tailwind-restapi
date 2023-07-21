add334327735f9a574e075f666afc98fdd78503a
saeed <saeedblack77@gmail.com>
2023-07-21 10:13:42 +0330

    khooneh

--- pages/index.js
+++ pages/index.js
@@ -82,13 +82,13 @@ export default function Example() {
                             <Transition.Child
                                 as={Fragment}
                                 enter="transition ease-in-out duration-300 transform"
-                                enterFrom="-translate-x-full"
+                                enterFrom="translate-x-full"
                                 enterTo="translate-x-0"
                                 leave="transition ease-in-out duration-300 transform"
                                 leaveFrom="translate-x-0"
-                                leaveTo="-translate-x-full"
+                                leaveTo="translate-x-full"
                             >
-                                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                                <Dialog.Panel className="relative ml-0 flex w-full max-w-xs flex-1">
                                     <Transition.Child
                                         as={Fragment}
                                         enter="ease-in-out duration-300"
@@ -98,7 +98,7 @@ export default function Example() {
                                         leaveFrom="opacity-100"
                                         leaveTo="opacity-0"
                                     >
-                                        <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                                        <div className="absolute right-full top-0 flex w-16 justify-center pt-5">
                                             <button type="button" className="-m-2.5 p-2.5"
                                                     onClick={() => setSidebarOpen(false)}>
                                                 <span className="sr-only">Close sidebar</span>
