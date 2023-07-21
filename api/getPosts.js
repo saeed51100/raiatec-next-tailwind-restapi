ee339951ce5daa4bf33d59f2f597de1a8789df1f
saeed <saeedblack77@gmail.com>
2023-07-21 20:57:33 +0330

    khooneh

--- api/getPosts.js
+++ api/getPosts.js
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function getPosts() {
-    const apiUrl = 'http://localhost/test.raiatec.com/wp-json/wp/v2/posts';
+    const apiUrl = 'http://localhost/test.raiatec.com/wp-json/wp/v2/posts?per_page=20';
     const { data, error } = useSWR(apiUrl, fetcher);
 
     return {
