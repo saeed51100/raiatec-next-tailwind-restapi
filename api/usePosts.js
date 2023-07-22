d558179e4714b4b29372d482eed9917acc6316ac
saeed <saeedblack77@gmail.com>
2023-07-22 19:53:19 +0330

    khooneh

--- api/usePosts.js
+++ api/usePosts.js
@@ -13,3 +13,11 @@ export function usePosts() {
         isError: error,
     };
 }
+
+// Function to fetch a single post by its slug
+export async function fetchPostBySlug(slug) {
+  const apiUrl = `http://localhost/test.raiatec.com/wp-json/wp/v2/posts?slug=${slug}`;
+  const res = await fetch(apiUrl);
+  const post = await res.json();
+  return post[0]; // Assuming the slug is unique, so we take the first post found
+}
--- pages/[post-slug].js
+++ pages/[post-slug].js
@@ -1,2 +1,33 @@
 // <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
 
+// pages/[post-slug].js
+import { useRouter } from 'next/router';
+import { usePosts } from '../api/usePosts';
+
+export default function PostPage() {
+    const router = useRouter();
+    const { postSlug } = router.query;
+
+    const { posts, isLoading, isError } = usePosts();
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Error fetching data.</div>;
+    }
+
+    const post = posts.find((post) => post.slug === postSlug);
+
+    if (!post) {
+        return <div>Post not found.</div>;
+    }
+
+    return (
+        <div>
+            <h1>{post.title.rendered}</h1>
+            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </div>
+    );
+}
