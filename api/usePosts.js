64d89bae45c64c8326354c782874d72bb432a8f2
saeed <saeedblack77@gmail.com>
2023-07-22 22:50:00 +0330

    khooneh

--- api/usePosts.js
+++ api/usePosts.js
@@ -1,8 +1,8 @@
 // api/usePosts.js
+
+// Function to fetch post titles
 import useSWR from 'swr';
-
 const fetcher = (url) => fetch(url).then((res) => res.json());
-
 export function usePosts() {
     const apiUrl = 'http://localhost/test.raiatec.com/wp-json/wp/v2/posts?per_page=20';
     const { data, error } = useSWR(apiUrl, fetcher);
