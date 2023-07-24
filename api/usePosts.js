ef35a5d533af2d80d4a0f323a67e4a2024eae445
saeed <saeedblack77@gmail.com>
2023-07-24 15:58:06 +0330

    khooneh

--- api/usePosts.js
+++ api/usePosts.js
@@ -1,16 +1,15 @@
 // api/usePosts.js
 
-// Function to fetch post titles
+
 import useSWR from 'swr';
+
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// Function to fetch post titles
 export function usePosts() {
-
-    // Use the API_URL from .env.local here
     const apiUrl = `${API_URL}/posts?per_page=20`;
-
-    const { data, error } = useSWR(apiUrl, fetcher);
-
+    const {data, error} = useSWR(apiUrl, fetcher);
     return {
         posts: data,
         isLoading: !error && !data,
@@ -18,11 +17,11 @@ export function usePosts() {
     };
 }
 
-// Function to fetch a single post by its slug using SWR
+// Function to fetch a single post by its slug
 export function usePostBySlug(slug) {
-  const apiUrl = `${API_URL}/posts?slug=${slug}`;
+    const apiUrl = `${API_URL}/posts?slug=${slug}`;
     console.log(apiUrl);
-    const { data, error } = useSWR(apiUrl, fetcher);
+    const {data, error} = useSWR(apiUrl, fetcher);
 
     return {
         post: data && data[0], // Assuming the slug is unique, so we take the first post found
