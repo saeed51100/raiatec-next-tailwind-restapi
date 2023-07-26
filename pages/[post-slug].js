688c033d0f6c452c08a45b8dbec5c5dad48e77ba
saeed <saeedblack77@gmail.com>
2023-07-26 22:54:22 +0330

    khooneh

--- pages/[post-slug].js
+++ pages/[post-slug].js
@@ -1,31 +1,29 @@
-// <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-
 // pages/[post-slug].js
-import { useRouter } from 'next/router';
-import { usePostBySlug } from '../api/usePosts';
 
-export default function Post() {
+import {useRouter} from 'next/router';
+import {usePostBySlug} from 'api/usePosts'; // Import the custom hook
+
+const SinglePostPage = () => {
     const router = useRouter();
-    console.log("router=", router)
-    const { post, isLoading, isError } = usePostBySlug(router.query.postSlug);
+    const {post, isLoading, isError} = usePostBySlug(router.query['post-slug']); // Use the custom hook with the dynamic slug from the router
 
     if (isLoading) {
         return <div>Loading...</div>;
     }
 
-    if (isError) {
-        return <div>Error loading post</div>;
-    }
-
-    if (!post) {
-        return <div>Post not found</div>;
+    if (isError || !post) {
+        return <div>Error loading the post.</div>;
     }
 
     return (
         <div>
             <h1>{post.title.rendered}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+            <div dangerouslySetInnerHTML={{__html: post.content.rendered}}/>
         </div>
     );
-}
+};
+
+export default SinglePostPage;
+
+
 
