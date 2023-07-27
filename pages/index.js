4162f2896e3a01596b5264649e771350bcfd23c2
saeed <saeedblack77@gmail.com>
2023-07-27 14:48:39 +0330

    khooneh

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
+// pages/index.js
 
 // import Navbar from 'components/navbar';
-import HomePage from 'components/grid';
+import Everyname from 'components/grid';
 // import Footer from 'components/footer';
 
 export default function IndexShow() {
     return (
         <div>
             {/*<Navbar />*/}
-            <HomePage />
+            <Everyname />
             {/*<Footer />*/}
         </div>
     );
