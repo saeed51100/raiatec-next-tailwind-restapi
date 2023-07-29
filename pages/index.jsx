2d4ca9c11f51b0578b37a74c0adec9992857b903
saeed <saeedblack77@gmail.com>
2023-07-29 13:02:41 +0330

    edare

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,49 +17,19 @@ import {Dialog, Menu, Transition} from '@headlessui/react'
 import {
     Bars3Icon,
     BellIcon,
-    CalendarIcon,
-    ChartPieIcon,
-    Cog6ToothIcon,
-    DocumentDuplicateIcon,
-    FolderIcon,
-    HomeIcon,
-    UsersIcon,
     XMarkIcon,
 } from '@heroicons/react/24/outline'
-import {ChevronDownIcon, MagnifyingGlassIcon, EnvelopeIcon, PhoneIcon} from '@heroicons/react/20/solid'
+import {ChevronDownIcon, MagnifyingGlassIcon} from '@heroicons/react/20/solid'
 import {usePosts} from "api/usePosts";
 import Link from "next/link";
 import ListOfPost from "../components/list-of-opst";
 
-const navigation = [
-    {name: 'Dashboard', href: '#', icon: HomeIcon, current: true},
-    {name: 'Team', href: '#', icon: UsersIcon, current: false},
-    {name: 'Projects', href: '#', icon: FolderIcon, current: false},
-    {name: 'Calendar', href: '#', icon: CalendarIcon, current: false},
-    {name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false},
-    {name: 'Reports', href: '#', icon: ChartPieIcon, current: false},
-]
-const teams = [
-    {id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false},
-    {id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false},
-    {id: 3, name: 'Workcation', href: '#', initial: 'W', current: false},
-]
+
 const userNavigation = [
     {name: 'Your profile', href: '#'},
     {name: 'Sign out', href: '#'},
 ]
-const people = [
-    {
-        name: 'Jane Cooper',
-        title: 'Paradigm Representative',
-        role: 'Admin',
-        email: 'janecooper@example.com',
-        telephone: '+1-202-555-0170',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    },
-    // More people...
-]
+
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
