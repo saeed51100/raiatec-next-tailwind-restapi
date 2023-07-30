28a7f69a07b8d3b99dc0cdc6dcfd4899c94d3227
saeed <saeedblack77@gmail.com>
2023-07-30 20:30:48 +0330

    khooneh

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,13 +13,13 @@
   ```
 */
 import {Fragment, useState} from 'react'
-import {Dialog, Menu, Transition} from '@headlessui/react'
+import {Dialog, Transition} from '@headlessui/react'
 import {
     Bars3Icon,
     BellIcon,
     XMarkIcon,
 } from '@heroicons/react/24/outline'
-import {ChevronDownIcon, MagnifyingGlassIcon} from '@heroicons/react/20/solid'
+import {MagnifyingGlassIcon} from '@heroicons/react/20/solid'
 import {usePosts} from "api/usePosts";
 import Link from "next/link";
 import ListOfPost from "../components/list-of-opst";
@@ -30,10 +30,6 @@ const userNavigation = [
     {name: 'Sign out', href: '#'},
 ]
 
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
 
 export default function Example() {
     const [sidebarOpen, setSidebarOpen] = useState(false)
@@ -108,7 +104,7 @@ export default function Example() {
                                                 alt="Your Company"
                                             />
                                         </div>
-                                        <ListOfPost />
+                                        <ListOfPost/>
                                     </div>
                                 </Dialog.Panel>
                             </Transition.Child>
@@ -151,54 +147,6 @@ export default function Example() {
                                 <BellIcon className="h-6 w-6" aria-hidden="true"/>
                             </button>
 
-                            {/* Separator */}
-                            <div className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-200" aria-hidden="true"/>
-
-                            {/* Profile dropdown */}
-                            <Menu as="div" className="relative">
-                                <Menu.Button className="-m-1.5 flex items-center p-1.5">
-                                    <span className="sr-only">Open user menu</span>
-                                    <img
-                                        className="h-8 w-8 rounded-full bg-gray-50"
-                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                        alt=""
-                                    />
-                                    <span className="hidden lg:flex lg:items-center">
-                      <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
-                        Tom Cook
-                      </span>
-                      <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true"/>
-                    </span>
-                                </Menu.Button>
-                                <Transition
-                                    as={Fragment}
-                                    enter="transition ease-out duration-100"
-                                    enterFrom="transform opacity-0 scale-95"
-                                    enterTo="transform opacity-100 scale-100"
-                                    leave="transition ease-in duration-75"
-                                    leaveFrom="transform opacity-100 scale-100"
-                                    leaveTo="transform opacity-0 scale-95"
-                                >
-                                    <Menu.Items
-                                        className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                                        {userNavigation.map((item) => (
-                                            <Menu.Item key={item.name}>
-                                                {({active}) => (
-                                                    <a
-                                                        href={item.href}
-                                                        className={classNames(
-                                                            active ? 'bg-gray-50' : '',
-                                                            'block px-3 py-1 text-sm leading-6 text-gray-900'
-                                                        )}
-                                                    >
-                                                        {item.name}
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        ))}
-                                    </Menu.Items>
-                                </Transition>
-                            </Menu>
                         </div>
                     </div>
                 </div>
@@ -216,7 +164,7 @@ export default function Example() {
                                 alt="Your Company"
                             />
                         </div>
-                        <ListOfPost />
+                        <ListOfPost/>
                     </div>
                 </div>
 
