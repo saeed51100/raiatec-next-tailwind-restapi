77974aa728253614242090aa29cefc56dbe56b3f
saeed <saeedblack77@gmail.com>
2023-08-03 21:08:22 +0330

    khooneh

--- components/menu-top.jsx
+++ components/menu-top.jsx
@@ -112,15 +112,18 @@ export default function MenuTop() {
 
                 {/*Navigation Items*/}
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul style={{ direction: 'initial' }} className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul style={{direction: 'initial'}}
+                        className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <link href="/"
-                               className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                               aria-current="page">Home</link>
+                            <Link href="/" legacyBehavior>
+                                <a className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                                   aria-current="page">Home</a>
+                            </Link>
                         </li>
                         <li>
-                            <link href="/about"
-                               className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</link>
+                            <Link href="/about" legacyBehavior>
+                                <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
