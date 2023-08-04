a3fff2b4b2a41dfcb3443e7531705f4f6448dd25
saeed <saeedblack77@gmail.com>
2023-08-04 21:11:50 +0330

    khooneh

--- components/menu-top.jsx
+++ components/menu-top.jsx
@@ -121,7 +121,7 @@ export default function MenuTop() {
             </Transition.Root>
 
             <div
-                className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+                className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white dark:bg-gray-900 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
 
                 {/*Raiatec logo */}
                 <div className="flex h-16 shrink-0 items-center">
@@ -220,3 +220,6 @@ export default function MenuTop() {
         </>
     )
 }
+
+
+
