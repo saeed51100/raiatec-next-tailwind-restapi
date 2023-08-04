a87ebdc59e4e4382c7ddec5e118551cfd3d2a689
saeed <saeedblack77@gmail.com>
2023-08-04 14:48:50 +0330

    khooneh

--- components/menu-top.jsx
+++ components/menu-top.jsx
@@ -103,10 +103,10 @@ export default function MenuTop() {
                 {/*Search Form*/}
                 <div className="flex flex-1 items-center justify-center px-2 lg:ml-6">
                     <div className="w-full max-w-lg lg:max-w-xs">
-                        <label for="search" className="sr-only">Search</label>
+                        <label form="search" className="sr-only">Search</label>
                         <div className="relative">
                             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                <MagnifyingGlassIcon class="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                                <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
                             </div>
                             <input id="search" name="search"
                                    className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
