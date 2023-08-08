b26ade0520be3f31636ca24843fa23b88d03b4ee
saeed <saeedblack77@gmail.com>
2023-08-08 20:50:05 +0330

    khooneh

--- components/list-of-post.jsx
+++ components/list-of-post.jsx
@@ -14,16 +14,12 @@ const ListOfPost = ({onClose}) => {
         return <div>Error loading posts.</div>;
     }
 
-    const handlePostClick = () => {
-        onClose(); // Call the onClose function to close the modal
-    };
-
     return (
         <div>
             {posts.map((post) => (
                 <div key={post.id} className="my-2">
 
-                    <Link href={`/${post.slug}`} onClick={handlePostClick}>
+                    <Link href={`/${post.slug}`} onClick={onClose}>
                         {post.title.rendered}
                     </Link>
 
