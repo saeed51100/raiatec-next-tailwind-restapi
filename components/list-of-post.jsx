9372e0c1e63b4d035438bdc444fa6179602e400a
saeed <saeedblack77@gmail.com>
2023-08-10 19:42:54 +0330

    khooneh

--- components/list-of-post.jsx
+++ components/list-of-post.jsx
@@ -1,11 +1,25 @@
 // components/list-of-posts.jsx
-
+import { useState, useMemo } from 'react';
 import {usePosts, useCategories} from "api/useApi"; // Import the custom hook
 import Link from "next/link";
 
 const ListOfPost = ({onClose}) => {
     const {posts, isLoading: isLoadingPosts, isError: isErrorPosts} = usePosts(); // Use the custom hook for posts
     const {categories, isLoading: isLoadingCategories, isError: isErrorCategories} = useCategories(); // Use the custom hook for categories
+
+// Computed property to check if category name is repeated
+    const isCategoryRepeated = useMemo(() => {
+        const uniqueCategories = new Set();
+        return (categoryName) => {
+            if (uniqueCategories.has(categoryName)) {
+                return true; // Category name is repeated
+            } else {
+                uniqueCategories.add(categoryName);
+                return false; // Category name is not repeated
+            }
+        };
+    });
+
 
     if (isLoadingPosts || isLoadingCategories) {
         return <div>Loading...</div>;
@@ -26,7 +40,7 @@ const ListOfPost = ({onClose}) => {
 
                         categories.map((category) => (
                             <div key={category.id}>
-                                {post.categories.includes(category.id) && isCategoryIsUnique(category.name) ? (
+                                {post.categories.includes(category.id) && !isCategoryRepeated(category.name) ? (
                                     <div className="bg-green-200">
                                         {/* category.name of related current post */}
                                         {category.name}
