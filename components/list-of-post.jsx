c13376cb2f5b10190a66bbfb8a48be03f887905f
saeed <saeedblack77@gmail.com>
2023-08-14 21:54:33 +0330

    khooneh

--- components/list-of-post.jsx
+++ components/list-of-post.jsx
@@ -1,10 +1,35 @@
 // components/list-of-posts.jsx
+import { useState } from 'react';
 import {usePosts, useCategories} from "api/useApi";
 import Link from "next/link";
 
-const ListOfPost = ({onClose}) => {
+export default function ListOfPost({ onClose }) {
     const {posts, isLoading: isLoadingPosts, isError: isErrorPosts} = usePosts();
     const {categories, isLoading: isLoadingCategories, isError: isErrorCategories} = useCategories();
+    const faqs = [
+        {
+            question: "question1",
+            answer: "answer1",
+        },
+        {
+            question: "question2",
+            answer: "answer2",
+        },
+        {
+            question: "question3",
+            answer: "answer3",
+        },
+    ];
+
+    const [openAccordion, setOpenAccordion] = useState(null);
+
+    const toggleAccordion = (index) => {
+        if (openAccordion === index) {
+            setOpenAccordion(null);
+        } else {
+            setOpenAccordion(index);
+        }
+    };
 
     if (isLoadingPosts || isLoadingCategories) {
         return <div>Loading...</div>;
@@ -22,6 +47,38 @@ const ListOfPost = ({onClose}) => {
 
 
             {/* Place the accordion here */}
+            <div className="space-y-4">
+                {faqs.map((faq, index) => (
+                    <div key={faq.question}>
+                        <div
+                            className="flex cursor-pointer list-none"
+                            onClick={() => toggleAccordion(index)}
+                        >
+                            <span>{faq.question} </span>
+                            <span className={`transition transform ${openAccordion === index ? '-rotate-90' : 'rotate-0'}`}>
+          <svg
+              fill="none"
+              height="24"
+              shapeRendering="geometricPrecision"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              width="24"
+          >
+            <path d="M15,6l-6,6l6,6"/>
+          </svg>
+        </span>
+                        </div>
+                        {openAccordion === index && (
+                            <p className="text-neutral-600 mt-3 animate-fadeIn">
+                                {faq.answer}
+                            </p>
+                        )}
+                    </div>
+                ))}
+            </div>
 
 
             {posts.map((post) => (
@@ -72,4 +129,3 @@ const ListOfPost = ({onClose}) => {
     );
 };
 
-export default ListOfPost;
