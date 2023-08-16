0fa3ba9802c87306a84f6953bcf7cfd68dab1d3b
saeed <saeedblack77@gmail.com>
2023-08-16 22:02:43 +0330

    khooneh

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,19 +1,15 @@
 import ListOfPost from "./list-of-post";
 
 export default function Sidebar() {
-    return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col mt-16">
-        {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-            <div className="flex h-16 shrink-0 items-center">
-                <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
-                />
-            </div>
-            <ListOfPost/>
-        </div>
+  return (<div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col mt-16">
+    {/* Sidebar component, swap this element with another sidebar if you like */}
+    <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+      <div className="flex h-16 shrink-0 items-center">
+        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+             alt="Your Company"
+        />
+      </div>
+      <ListOfPost />
     </div>
-    )
+  </div>);
 }
