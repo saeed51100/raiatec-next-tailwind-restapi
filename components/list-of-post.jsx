b1993588a501298eaf9ae1d045e9abf2088825b6
saeed <saeedblack77@gmail.com>
2023-08-19 21:00:25 +0330

    khooneh

--- components/list-of-post.jsx
+++ components/list-of-post.jsx
@@ -40,7 +40,6 @@ export default function ListOfPost({ onClose }) {
     <div>
       {/* Place the accordion here */}
 
-
       {posts.map((post) => (
         <div key={post.id} className="my-2">
           {post.categories.length === 0 ? (
@@ -48,14 +47,6 @@ export default function ListOfPost({ onClose }) {
               {post.title.rendered}
             </Link>
           ) : (
-
-
-
-
-
-
-
-
             post.categories.map((categoryId, index) => {
               const category = categories.find((cat) => cat.id === categoryId);
 
@@ -63,58 +54,57 @@ export default function ListOfPost({ onClose }) {
                 uniqueCategoryNames.add(category.name);
                 return (
                   <div key={category.id}>
-                    <div className="flex cursor-pointer list-none" onClick={() => toggleAccordion(index)}>
-                    <div className="bg-green-200">
-                      {category.name}
-                    </div>
-                      <span className={`transition transform ${openAccordion === index ? "-rotate-90" : "rotate-0"}`}>
-                <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24">
-                  <path d="M15,6l-6,6l6,6" />
-                </svg>
-              </span>
+                    <div
+                      className="flex cursor-pointer list-none"
+                      onClick={() => toggleAccordion(index)}
+                    >
+                      <div className="bg-green-200">{category.name}</div>
+                      <span
+                        className={`transition transform ${
+                          openAccordion === index ? "-rotate-90" : "rotate-0"
+                        }`}
+                      >
+                        <svg
+                          fill="none"
+                          height="24"
+                          shapeRendering="geometricPrecision"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="1.5"
+                          viewBox="0 0 24 24"
+                          width="24"
+                        >
+                          <path d="M15,6l-6,6l6,6" />
+                        </svg>
+                      </span>
                     </div>
                     {/* related Post titles */}
 
-
-
                     {openAccordion === index && (
                       <p className="text-neutral-600 mt-3 animate-fadeIn">
-                    {posts
-                      .filter((postItem) => postItem.categories.includes(category.id))
-                      .sort((a, b) => a.id - b.id)
-                      .map((postItem) => (
-
-
-
-                          <Link
-                            key={postItem.id}
-                            href={`/${postItem.slug}`}
-                            onClick={onClose}
-                          >
-
-                            {postItem.title.rendered}
-
-                          </Link>
-
-
-
-
-                      ))}
-                    </p>)}
+                        {posts
+                          .filter((postItem) =>
+                            postItem.categories.includes(category.id)
+                          )
+                          .sort((a, b) => a.id - b.id)
+                          .map((postItem) => (
+                            <ul key={postItem.id}>
+                              <Link
+                                href={`/${postItem.slug}`}
+                                onClick={onClose}
+                              >
+                                {postItem.title.rendered}
+                              </Link>
+                            </ul>
+                          ))}
+                      </p>
+                    )}
                   </div>
                 );
               }
               return null; // Category name already displayed, don't render
             })
-
-
-
-
-
-
-
-
-
           )}
         </div>
       ))}
