875d8ef502e4bdb0803eac516eb33100346c2505
saeed <saeedblack77@gmail.com>
2023-08-21 18:00:41 +0330

    khooneh

--- components/list-of-post.jsx
+++ components/list-of-post.jsx
@@ -68,34 +68,24 @@ export default function ListOfPost({ onClose }) {
     <div>
       {/* Sidebar component, swap this element with another sidebar if you like */}
 
+      <ul role="list" className="-mx-2 space-y-1">
+        {navigation.map((item) => (
+          <li key={item.name}>
+            <a
+              href={item.href}
+              className={classNames(
+                item.current
+                  ? "bg-gray-50 text-indigo-600"
+                  : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+              )}
+            >
 
-          <ul role="list" className="-mx-2 space-y-1">
-            {navigation.map((item) => (
-              <li key={item.name}>
-                <a
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-50 text-indigo-600"
-                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                  )}
-                >
-                  <item.icon
-                    className={classNames(
-                      item.current
-                        ? "text-indigo-600"
-                        : "text-gray-400 group-hover:text-indigo-600",
-                      "h-6 w-6 shrink-0"
-                    )}
-                    aria-hidden="true"
-                  />
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
 
       <br />
       <br />
