da02bd7ab131a1594719eeb95a70c69050c1b0af
saeed <saeedblack77@gmail.com>
2023-08-22 09:15:27 +0330

    edare

--- components/list-of-post.jsx
+++ components/list-of-post.jsx
@@ -2,35 +2,12 @@
 import { useState } from "react";
 import { usePosts, useCategories } from "api/useApi";
 
-import {
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-} from "@heroicons/react/24/outline";
-
-const navigation = [
-  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
-];
-const teams = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
-];
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 export default function ListOfPost({ onClose }) {
   const {
@@ -68,38 +45,22 @@ export default function ListOfPost({ onClose }) {
 
   const linkActive = "bg-gray-50 text-indigo-600";
   const linkHover = "text-gray-700 hover:text-indigo-600 hover:bg-gray-50";
-  const addJoin = "";
+  const addJoin =
+    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold";
   return (
     <div>
-      {/* Sidebar component, swap this element with another sidebar if you like */}
-
-      <ul role="list" className="-mx-2 space-y-1">
-        {navigation.map((item) => (
-          <li key={item.name}>
-            <a
-              href={item.href}
-              className={classNames(
-                item.current
-                  ? "bg-gray-50 text-indigo-600"
-                  : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-              )}
-            >
-
-              {item.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-
-      <br />
-      <br />
-      <br />
-
       {posts.map((post) => (
         <div key={post.id} className="my-2">
           {post.categories.length === 0 ? (
-            <Link href={`/${post.slug}`} onClick={onClose}>
+            <Link
+              key={post.id}
+              href={`/${post.slug}`}
+              onClick={onClose}
+              className={classNames(
+                router.asPath === `/${post.slug}` ? linkActive : linkHover,
+                addJoin
+              )}
+            >
               {post.title.rendered}
             </Link>
           ) : (
@@ -152,10 +113,10 @@ export default function ListOfPost({ onClose }) {
                               href={`/${postItem.slug}`}
                               onClick={onClose}
                               className={classNames(
-                                  router.asPath === `/${postItem.slug}`
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                router.asPath === `/${postItem.slug}`
+                                  ? linkActive
+                                  : linkHover,
+                                addJoin
                               )}
                             >
                               <div>{postItem.title.rendered}</div>
