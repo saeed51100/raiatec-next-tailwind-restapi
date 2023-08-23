c089ec0883ea708595ffa35b183878350b5e8b84
saeed <saeedblack77@gmail.com>
2023-08-23 21:11:40 +0330

    khooneh

--- components/raiatec-logo.jsx
+++ components/raiatec-logo.jsx
@@ -4,6 +4,7 @@ export default function RaiatecLogo() {
   return (
     <div className="flex h-16 shrink-0 items-center">
       <Link href="/">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/raiatec.svg" alt="Raiatec Logo" className="h-8 w-auto" />
       </Link>
     </div>
