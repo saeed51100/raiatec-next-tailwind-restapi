9bfa11c22ed71afe0171bd99995fdc824b756c59
saeed <saeedblack77@gmail.com>
2023-08-24 18:49:53 +0330

    khooneh

--- components/grid.jsx
+++ components/grid.jsx
@@ -19,9 +19,7 @@ export default function Grid() {
       >
         {posts.map((post) => (
           <div key={post.id} className="max-w-lg mx-auto">
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md mb-5 dark:bg-gray-800 dark:border-gray-700">
-            {/*<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">*/}
-
+            <div className="max-w-sm bg-white rounded-lg shadow-md mb-5 dark:bg-gray-800 dark:border-gray-700">
               {/* Check if the featured media is available */}
               {post._embedded &&
               post._embedded["wp:featuredmedia"] &&
@@ -44,9 +42,7 @@ export default function Grid() {
                 />
               )}
               <div className="p-5">
-                <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                    // className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                >
+                <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   <Link href={`/${post.slug}`}>{post.title.rendered}</Link>
                 </h3>
 
@@ -54,7 +50,6 @@ export default function Grid() {
                 {post.excerpt && (
                   <p
                     className="mb-3 font-normal text-gray-700 line-clamp-4 break-all dark:text-gray-400"
-                    // className="mb-3 font-normal text-gray-700 dark:text-gray-400"
                     dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
                   />
                 )}
@@ -64,7 +59,6 @@ export default function Grid() {
                   {post.slug && (
                     <Link
                       className="inline-flex items-center px-3 py-2 text-sm font-medium             text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4                    focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 absolute bottom-0 left-0 text-center"
-                      // className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                       href={`/${post.slug}`}
                     >
                       {"Read More"}
@@ -73,8 +67,7 @@ export default function Grid() {
                 </div>
               </div>
             </div>
-
-            </div>
+          </div>
         ))}
       </section>
     </>
