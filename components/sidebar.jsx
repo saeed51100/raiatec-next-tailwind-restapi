0ee769ffd5ead57d808f640e4aa3f7558949a60d
saeed <saeedblack77@gmail.com>
2023-08-24 09:41:01 +0330

    edare

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col mt-16">
       {/* Sidebar component, swap this element with another sidebar if you like */}
-      <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white dark:bg-gray-800 px-2 pb-4 overflow-y-scroll">
+      <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-white dark:border-gray-800 bg-white dark:bg-gray-800 px-2 pb-4 overflow-y-scroll">
         <ListOfPost />
       </div>
     </div>
