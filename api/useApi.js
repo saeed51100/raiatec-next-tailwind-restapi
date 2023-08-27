0fb803853974cbead425ee7264a2c54be7526943
saeed <saeedblack77@gmail.com>
2023-08-27 10:00:18 +0330

    edare

--- api/useApi.js
+++ api/useApi.js
@@ -38,3 +38,15 @@ export function useCategories() {
     isError: error
   };
 }
+
+// Function to fetch search result
+export function useSearch(keyword) {
+  const apiUrl = `${API_URL}/posts?search=${encodeURIComponent(keyword)}`;
+  const { data, error } = useSWR(apiUrl, fetcher);
+
+  return {
+    results: data,
+    isLoading: !error && !data,
+    isError: error
+  };
+}
