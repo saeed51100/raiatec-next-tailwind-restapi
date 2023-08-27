6828827675dcd893beac85e2e17159e12815ce54
saeed <saeedblack77@gmail.com>
2023-08-27 10:51:55 +0330

    edare

--- components/search-Form.jsx
+++ components/search-Form.jsx
@@ -24,28 +24,31 @@ export default function SearchForm() {
             />
           </div>
           <input
-            id="search"
-            name="search"
             className="block w-full rounded-md border border-gray-300 bg-gray-50 py-1.5 pl-10 pr-3 text-gray-900 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:text-sm sm:leading-6"
             placeholder="Search"
-            type="search"
             value={searchTerm}
             onChange={handleInputChange}
           />
-          {searchTerm && results && (
-            <div>
-              {results.map((result) => (
-                <Link key={result.id} href={`/${result.slug}`}>
-                  {result.title.rendered}
-                </Link>
-              ))}
-            </div>
-          )}
+
+          <div className="absolute z-10 w-full border divide-y shadow max-h-72 overflow-y-auto bg-white ...">
+            {/*<a className="block p-2 hover:bg-indigo-50 ..." href="#">Tailwind</a>*/}
+            {/*<a className="block p-2 hover:bg-indigo-50 ..." href="#">Bootstrap</a>*/}
+            {searchTerm && results && (
+              <div>
+                {results.map((result) => (
+                  <Link
+                    className="block p-2 hover:bg-indigo-50"
+                    key={result.id}
+                    href={`/${result.slug}`}
+                  >
+                    {result.title.rendered}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-
-
