c05ff72c4705797c2c7b8b5f3992d443e2e6d3c9
saeed <saeedblack77@gmail.com>
2023-08-28 18:30:23 +0330

    khooneh

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,8 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+  return (
+    <div className="container st404">
+      <h1 style="font-size: 100px">404</h1>
+      <h1>صفحه مورد نظر پیدا نشد</h1>
+    </div>
+  );
 }
