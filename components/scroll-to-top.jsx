8047b091fabe814eb073af3a5dc62e96dbaa3efa
saeed <saeedblack77@gmail.com>
2023-08-28 17:46:03 +0330

    khooneh

---
+++ components/scroll-to-top.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    isVisible && (
+      <div
+        onClick={scrollToTop}
+        className="fixed bottom-8 left-4 rounded-full cursor-pointer z-90 bg-indigo-500 hover:bg-indigo-400 text-3xl flex items-center justify-center w-12 h-12"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="white"
+          strokeWidth="3"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M5 10l7-7m0 0l7 7m-7-7v18"
+          />
+        </svg>
+      </div>
+    )
+  );
+};
+
+export default ScrollToTopButton;
