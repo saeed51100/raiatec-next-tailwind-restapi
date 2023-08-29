d3ef7cd1425a3123b8f9db372dc725d30417bbc7
saeed <saeedblack77@gmail.com>
2023-08-29 23:07:36 +0330

    khooneh

--- components/grid.jsx
+++ components/grid.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { usePosts } from "api/useApi";
-import Image from "next/image";
 
 export default function Grid() {
   const { posts, isLoading, isError } = usePosts(); // Use the custom hook
@@ -20,27 +19,7 @@ export default function Grid() {
         {posts.map((post) => (
           <div key={post.id} className="max-w-lg mx-auto">
             <div className="max-w-sm bg-white rounded-lg shadow-md mb-5 dark:bg-gray-800 dark:border-gray-700">
-              {/* Check if the featured media is available */}
-              {post._embedded &&
-              post._embedded["wp:featuredmedia"] &&
-              post._embedded["wp:featuredmedia"][0].source_url ? (
-                <Image
-                  width={500}
-                  height={500}
-                  src={post._embedded["wp:featuredmedia"][0].source_url}
-                  alt={post.title.rendered}
-                  className="rounded-t"
-                />
-              ) : (
-                <Image
-                  width={500}
-                  height={500}
-                  src="/raiatecThumb.svg"
-                  alt={post.title.rendered}
-                  style={{ width: "100%" }}
-                  className="rounded-t"
-                />
-              )}
+
               <div className="p-5">
                 <h3 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
                   <Link href={`/${post.slug}`}>{post.title.rendered}</Link>
@@ -55,16 +34,7 @@ export default function Grid() {
                 )}
                 <br />
                 <br />
-                <div className="relative">
-                  {post.slug && (
-                    <Link
-                      className="font-bold text-green-400 focus:ring-4 absolute bottom-0 left-0 px-3 py-2"
-                      href={`/${post.slug}`}
-                    >
-                      {"ادامه مطلب..."}
-                    </Link>
-                  )}
-                </div>
+
               </div>
             </div>
           </div>
