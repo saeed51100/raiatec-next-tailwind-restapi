710f2c596a461ba51d489b50cf350ba2820e230a
saeed <saeedblack77@gmail.com>
2023-08-30 10:24:35 +0330

    edare

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,15 +1,8 @@
 import { usePosts } from "api/useApi";
-import Sidebar from "components/sidebar";
-import React, { useState, useCallback } from "react";
-import Navbar from "components/navbar";
 
 import Custom404 from "pages/404";
 
 export default function Layout({ children }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const openSidebar = useCallback(() => setSidebarOpen(true), []);
-  const closeSidebar = useCallback(() => setSidebarOpen(false), []);
   const { isLoading, isError } = usePosts(); // Use the custom hook
   if (isLoading) {
     return <div>Loading...</div>;
@@ -21,10 +14,6 @@ export default function Layout({ children }) {
 
   return (
     <div>
-
-
-      <Sidebar />
-
       <div className="lg:pr-72">
         {/*Your content */}
         <main className="">{children}</main>
