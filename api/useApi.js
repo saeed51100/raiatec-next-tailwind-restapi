b1d2743cbcfb7e495921826a6598a47a2e9a8052
saeed <saeedblack77@gmail.com>
2023-08-30 10:58:36 +0330

    edare

--- api/useApi.js
+++ api/useApi.js
@@ -5,8 +5,8 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Function to fetch post titles
-export function usePosts() {
-  const apiUrl = `${API_URL}/posts?per_page=20&_embed`;
+export function usePosts(page) {
+  const apiUrl = `${API_URL}/posts?per_page=5&page=${page}&_embed`;
   const { data } = useSWR(apiUrl, fetcher);
   return {
     posts: data,
--- components/grid.jsx
+++ components/grid.jsx
@@ -1,22 +1,31 @@
 // components/grid.jsx
+import { useState } from 'react';
 import Link from "next/link";
 import { usePosts } from "api/useApi";
 
 export default function Grid() {
-  const { posts } = usePosts(); // Use the custom hook
+  const [currentPage, setCurrentPage] = useState(1);
+  const { posts} = usePosts(currentPage); // Use the custom hook
+
+
 
   return (
     <>
       {posts?.map((post) => (
         <div key={post.id} className="max-w-lg mx-auto">
           <Link href={`/${post.slug}`}>{post.title.rendered}</Link>
-
           <br />
           <br />
           <br />
           <br />
         </div>
       ))}
+      <div>
+        {currentPage > 1 && (
+          <button onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+        )}
+        <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+      </div>
     </>
   );
 }
