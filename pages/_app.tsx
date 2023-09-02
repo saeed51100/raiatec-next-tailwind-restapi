0b0a8e8ddecaef164a49608dadbdf80e74ccff8b
saeed <saeedblack77@gmail.com>
2023-09-02 19:42:34 +0330

    khooneh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from 'next/app'
 import "../src/global.css";
 import Layout from "../components/layout";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layout>
